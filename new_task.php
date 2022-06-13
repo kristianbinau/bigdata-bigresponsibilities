ac36530807c7769ba9ccdfc5fb5d6c0efda0019c
Kristian Binau <kristian.insecure@protonmail.com>
2022-06-13 14:17:39 +0200

    dsd

--- new_task.php
+++ new_task.php
@@ -4,20 +4,34 @@ require_once __DIR__ . '/start.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+
+pcntl_signal(SIGINT,"shutdown");
+
 $connection = new AMQPStreamConnection('192.168.88.128', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
 $channel->queue_declare('task_queue', false, true, false, false);
 
-$data = rand(0, 10);
-$msg = new AMQPMessage(
-    $data,
-    array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
-);
+function shutdown(){
+    global $connection, $channel;
+    $channel->close();
+    $connection->close();
+};
 
-$channel->basic_publish($msg, '', 'task_queue');
 
-echo ' [x] Sent ', $data, "\n";
+while(true) {
+    $data = rand(0, 10) / 10;
+    $msg = new AMQPMessage(
+        $data,
+        array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
+    );
+    
+    $channel->basic_publish($msg, '', 'task_queue');
+    
+    echo ' [x] Sent ', $data, "\n";
 
-$channel->close();
-$connection->close();
+    sleep(0.3);
+}
+
+
+
