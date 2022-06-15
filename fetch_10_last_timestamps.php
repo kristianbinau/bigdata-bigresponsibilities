4fe1baae2bc7fc63c21a6d56e144a1d55060a464
Kristian Binau <kristian.insecure@protonmail.com>
2022-06-15 09:49:17 +0200

    ds

--- fetch_10_last_timestamps.php
+++ fetch_10_last_timestamps.php
@@ -4,19 +4,24 @@ require_once __DIR__ . '/start.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+// Setup connection to RabbitMQ
 $connection = new AMQPStreamConnection('192.168.88.128', 5672, 'guest', 'guest');
 $channel = $connection->channel();
-
 $channel->queue_declare('task_queue', false, true, false, false);
 
+
+// Create message.
 $msg = new AMQPMessage(
     '',
     array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
 );
 
+// Send message.
 $channel->basic_publish($msg, '', 'task_queue');
 
-echo " [x] Sent the request \n";
+echo ' [x] Sent ', 'empty string', "\n";
 
+
+// Close connections.
 $channel->close();
 $connection->close();
