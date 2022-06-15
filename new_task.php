ed67d13bf07bfd2ec9bcac88cdfb74ffc3d8f5e0
Kristian Binau <kristian@binau.me>
2022-06-15 15:48:58 +0200

    More work2

--- new_task.php
+++ new_task.php
@@ -14,7 +14,12 @@ $channel->queue_declare('task_queue', false, true, false, false);
  * Send a value between 0 and 1000000.
  */
 for($i = 0 ; $i < 10000 ; $i++) {
-    $data = mt_rand(0, 10000);
+    try {
+        $data = random_int(0, 10000);
+    } catch (Exception $e) {
+        echo ' Appropriate source of randomness cannot be found, using Mersenne Twister instead.', "\n";
+        $data = mt_rand(0, 10000);
+    }
 
     // Create message.
     $msg = new AMQPMessage(
--- worker.php
+++ worker.php
@@ -11,11 +11,65 @@ $channel->queue_declare('task_queue', false, true, false, false);
 
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
+/**
+ * Checks if a number is prime
+ *
+ * @param $num
+ * @return bool
+ */
+function IsPrime($num): bool
+{
+    if ($num < 2) {
+        return false;
+    }
+    for ($i = 2; $i <= $num / 2; $i++) {
+        if ($num % $i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/**
+ * Recursively solves The Fibonacci Sequence
+ * Max is the highest allowed value to be returned.
+ *
+ * @param int $max
+ * @param array $previous
+ * @param int $actual
+ * @return int[]
+ * @see https://en.wikipedia.org/wiki/Fibonacci_number
+ */
+function fibonacciThis(int $max, array $previous = [0], int $actual = 0): array
+{
+    $maxIntSize = PHP_INT_SIZE === 4 ? 1073741823 : 4611686000000000000;
+
+    if ($max > $maxIntSize) {
+        return fibonacciThis($maxIntSize, $previous, $actual);
+    }
+
+    $result = end($previous) + $actual;
+    $previous[] = $actual;
+
+    if ($result > $max) {
+        return $previous;
+    }
+
+    return fibonacciThis($max, $previous, $result?:1);
+}
+
 $callback = static function ($msg) {
     switch($msg) {
         case 'last10':
             // Fetch and return 10 last timestamps
             Capsule::table('logging_of_timestamps')->latest('end')->take(10)->get();
+            break;
+        case 'fibonacci':
+            fibonacciThis(4611686000000000000);
+            break;
+        case 'prime':
+            isPrime(99194853094755497);
             break;
         default:
             $start = new DateTime();
